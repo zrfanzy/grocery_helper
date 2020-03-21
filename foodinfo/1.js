d28f21e56509a8238ffb289f4892b6588ff64df2
Zhou <zrfanzy@gmail.com>
2020-03-21 15:31:17 +0100

    demo

--- foodinfo/1.js
+++ foodinfo/1.js
@@ -1,6 +1,6 @@
 var foodinfo = {
 	"name": "Pot-au-feu",
-	"keyword": ["beef", "leek", "caggage"],
+	"keyword": ["beef", "leek", "cabbage"],
 	"ingredients": "300-400g beef(bouille), 2x marrowbone, 0.5x cabbage, 0.5x carrot, 0.5x leek, 0.5x celery root, 1x onion",
 	"duration": "2 hrs"
 }
---
+++ foodinfo/7.js
@@ -0,0 +1,6 @@
+var foodinfo={
+	"name": "Stir-fried tomato and scrambled eggs (番茄炒蛋)",
+	"keyword": ["tomato","egg"],
+	"ingredients": "2-3x tomato, 2-3x egg",
+	"duration": "10 mins"
+}
