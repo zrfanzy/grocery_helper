6d715b4bba0b3f299870d47d886827280eb3e8e8
Zhou <zrfanzy@gmail.com>
2020-03-21 12:48:06 +0100

    test url parameter

---
+++ foodinfo/1.js
@@ -0,0 +1,6 @@
+var foodinfo={
+	"name": "Pot-au-feu",
+	"keyword": ["beef", "leek", "caggage"],
+	"ingredients": "beef(bouille) 300-400g, 2x marrowbone, 0.5x cabbage, 0.5x carrot, 0.5x leek, 0.5x celery root, 1x onion",
+	"duration": "2 hrs"
+}
---
+++ foodinfo/2.js
@@ -0,0 +1,6 @@
+var foodinfo={
+	"name": "Mille-Feuille Nabe (重ね鍋) ",
+	"keyword": ["pork", "cabbage"],
+	"ingredients": "pork belly/bacon 200g, 1x chinese cabbage",
+	"duration": "30 mins"
+}
